92645cbf2c0b4269bad5a5f5865ff6071e87b836
hyunjik.bae <hyunjik.bae@nettention.com>
2016-12-08 22:19:20 +0900

    del

--- CharacterMove-single/SimpleUnityClient/Prework-dev.ps1
+++
@@ -1,4 +0,0 @@
-﻿xcopy ..\..\..\srcNative\Bin\ProudDotNetClient.dll .\SimpleUnityClient\Assets\Plugins\ /d
-xcopy ..\..\..\srcNative\Bin\ProudNetClientPlugin.dll .\SimpleUnityClient\Assets\Plugins\ /d
-
-xcopy ..\SimpleCommon\simple*.cs .\SimpleUnityClient\Assets\ /d
